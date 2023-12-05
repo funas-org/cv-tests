8f135aa28b40ff6dde152da3c8da11ce6e6f87e6
leofuna <leo.funa@gmail.com>
2023-12-05 18:18:48 -0300

    testing debug

--- content/about.ts
+++ content/about.ts
@@ -21,7 +21,7 @@ const aboutContent: AboutContent = {
     content: 'We\'re empowering individuals with criminal records in Washington State to build brighter futures for themselves with help from our Eligibility Calculator.',
   },
   hero: {
-    header: 'About Us',
+    header: 'About TEST',
     body: "Clearviction was founded in 2020 sparked by [Washington State's New Hope Act](https://app.leg.wa.gov/billsummary?BillNumber=2890&Year=2017), We are a 501(c)(3) non-profit organization committed to helping people with convictions improve their quality of life. ",
     subheading2: 'Each year, nearly 98k people are added to the millions across Washington State already burdened with a criminal record. The justice-impacted folks will face significant barriers to their day to day life after completing their prison terms.',
     imgsrc: 'illustrations/map.svg',
